676fbdf7f87e116137fc5910547b64459a3f082c
Jerald Limqueco <jeraldlimqueco@yahoo.com>
2015-11-28 16:53:54 +0800

    weekday improved

---
+++ app/src/main/java/com/example/jerald/weekday3/MyInterface.java
@@ -0,0 +1,14 @@
+package com.example.jerald.weekday3;
+
+import android.widget.TextView;
+
+/**
+ * Created by Jerald on 11/27/2015.
+ */
+public interface MyInterface {
+    public void pickDateDisplayDay();
+
+    public void setText(String message);
+
+    public void fadeAnimation(TextView tv);
+}
